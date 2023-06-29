80eebba7814fa2f3355f6039d12e29ed6fe3b927
Vikas Singh <xanderbilla@gmail.com>
2023-06-29 20:53:02 +0300

    C12# Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 /*
-The useSearchParams hook is used to read and modify the query string in the URL for the current location. 
+Lazy loading is a technique where components are not required on the
+homepage can be split into seperate code bundle and downloaded when
+the user navigates to that page
 
-Searchparams scenario img
+It helps reduce initial load time thereby improving reload time 
 
+To lazy load the components we need to use dynamic import and react suspense
+
+Step 1: Import react component
+Step 2: Import the component (on which we want to apply lazy loading) by passing the the component export as an argument of Lazy function
+
+Lazing is a funciton that takes another function as an argument 
 */
+
+
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
+// import About from './pages/About'
 import Navbar from './components/Navbar'
 import OrderSummary from './components/OrderSummary'
 import Error from './components/Error'
@@ -16,6 +27,16 @@ import Featured from './components/Featured'
 import Users from './components/Users'
 import UserDetail from './components/UserDetail'
 import Admin from './components/Admin'
+const LazyAbout = React.lazy(() => import('./pages/About'))
+
+/*
+
+This React.Lazy return a promise by this dynamic import that later converted into a module that contain a default exported react component which of course is our About component (in this example)
+
+But to avoid the react component suspension after lazy loading that is nothing but when we use lazy loading for any component that component loads when we nabigate but suspended from rendering if we don't use suspension
+
+To apply suspense wrap the lazyload component with React.Suspense. on react suspense we have to specify a fallback prop which can be any react element let's say `loading...` 
+*/
 
 const App = () => {
   return (
@@ -23,7 +44,12 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
+        {/* Replace About with LazyAbout */}
+        <Route path='about' element={
+          <React.Suspense fallback='Loading...'>
+            <LazyAbout />
+          </React.Suspense>
+        } />
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='users' element={<Users />} >
           <Route path=':userId' element={<UserDetail />} />
@@ -41,9 +67,9 @@ const App = () => {
 }
 
 /*
-For Lazy Loading
+For Authentication
 
-~Refer to commit C12# 
+~Refer to commit C13# 
 */
 
 export default App
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,18 +2,6 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 const Products = () => {
-
-    /*
-        A relative link is a link that doesn't start with the forward slash and inherit the closet route in which they are rendered
-
-        Here features will append /featured to /products and new will append /new to /products/new
-
-        If we use absolute path instead of relative path then the featured and new will construct the path from the root of the app and not the current URL.
-
-        If the path is absolute it will open as localhost:3000/new instead of localhost:3000/products/new
-    */
-
-
     return (
         <>
             <div>
