cb06a2b52477d602433e9c09462159945a936ccd
Vikas Singh <xanderbilla@gmail.com>
2023-06-29 11:50:56 +0300

    C5# 404 Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 /*
-Let's say we want to navigate to different route based on some event. For example you are placing an order on an e-commerce application where you clicked on Place Order button and on successful you must be routed to Order Successful page.
-
-To achieve such functionality we use useNavigate renderHook
+What if there is no route that user want to access but there is no such URL. We must let user to know that there is no such route exist.
 */
 
 import { Routes, Route } from 'react-router-dom'
@@ -9,6 +7,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './components/Navbar'
 import OrderSummary from './components/OrderSummary'
+import Error from './components/Error'
 
 const App = () => {
   return (
@@ -18,15 +17,22 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='order-summary' element={<OrderSummary />} />
+      <Route path='*' element={<Error />} />
+      
+        {/* 
+        
+        * this route element will only render when there will be no matching route
+
+        */}
     </Routes>
     </>
   )
 }
 
 /*
-For No match rotuing or 404 Page
+For Nested Routes
 
-~Refer to commit C5# to use Link
+~Refer to commit C6# to use Link
 */
 
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+  
   const navigate = useNavigate()
   
-  /*
-  
-  we are going to use the navigate function based on button click event
- 
-  To replace the history instead of pushing a route onto the stack for that we can pass the second optional argument which is an object with property 'replace' and it's value true
-
-  */
   return (
     <>
       <div>Home Page</div>
